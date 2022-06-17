fad2997441f02385a0f9be89101e626604859575
kavinteng <76694895+kavinteng@users.noreply.github.com>
2022-06-17 16:43:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import os
+from threading import Thread
 
 def confirm_yesno(message = 'ยืนยันที่จะปิดโปรแกรมหรือไม่'):
     if messagebox.askyesno(title='confirmation',message=message):
@@ -15,16 +16,20 @@ def click_cam(num):
     elif num == 3:
         os.system('python person_tracking.py rtsp_subtype')
 
+def click_cam_thread(num):
+    t = Thread(target=click_cam, args=(num,))
+    t.start()
+
 if __name__ == '__main__':
     root = Tk()
     root.title('Application Controller')
     root.geometry('250x300+0+0')
 
-    cam1 = Button(root, text="cam1", width=20, bg='red', fg='white', command=lambda num=1: click_cam(num))
+    cam1 = Button(root, text="cam1", width=20, bg='red', fg='white', command=lambda num=1: click_cam_thread(num))
     cam1.pack(padx=5, pady=5)
-    cam2 = Button(root, text="cam2", width=20, bg='red', fg='white', command=lambda num=2: click_cam(num))
+    cam2 = Button(root, text="cam2", width=20, bg='red', fg='white', command=lambda num=2: click_cam_thread(num))
     cam2.pack(padx=5, pady=5)
-    cam3 = Button(root, text="cam3", width=20, bg='red', fg='white', command=lambda num=3: click_cam(num))
+    cam3 = Button(root, text="cam3", width=20, bg='red', fg='white', command=lambda num=3: click_cam_thread(num))
     cam3.pack(padx=5, pady=5)
 
     # admin = Button(root, text="ADMIN", width=20, bg='red', fg='white', command=admin_control)

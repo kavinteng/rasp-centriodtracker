0d3b3838bb440aa367845302925109da488f640d
kavinteng <76694895+kavinteng@users.noreply.github.com>
2022-06-17 15:20:47 +0700

    Add files via upload

--- download_model.py
+++ download_model.py
@@ -4,3 +4,7 @@ import os
 if os.path.isdir('camcount') == False:
     url = 'https://drive.google.com/drive/folders/15raFceARgdxrvEN8sfVKrRrmkEPNhroo'
     gdown.download_folder(url)
+
+if os.path.isdir('gender_age_model') == False:
+    url = "https://drive.google.com/drive/u/1/folders/1n7WSJV0CdGY8vaPukLxZDXT3TjzEZ-Hp"
+    gdown.download_folder(url)
--- person_tracking.py
+++ person_tracking.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from centroidtracker import CentroidTracker
+import time
 
 def non_max_suppression_fast(boxes, overlapThresh):
     try:
@@ -41,59 +42,135 @@ def non_max_suppression_fast(boxes, overlapThresh):
     except Exception as e:
         print("Exception occurred in non_max_suppression : {}".format(e))
 
+def load_all_model():
+    global MODEL_MEAN_VALUES,ageList,genderList,faceNet,ageNet,genderNet,model
+
+    print('load gender & age model')
+    faceProto="gender_age_model/opencv_face_detector.pbtxt"
+    faceModel="gender_age_model/opencv_face_detector_uint8.pb"
+    ageProto="gender_age_model/age_deploy.prototxt"
+    ageModel="gender_age_model/age_net.caffemodel"
+    genderProto="gender_age_model/gender_deploy.prototxt"
+    genderModel="gender_age_model/gender_net.caffemodel"
+
+    MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+    ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    genderList=['Male','Female']
+
+    faceNet=cv2.dnn.readNet(faceModel,faceProto)
+    ageNet=cv2.dnn.readNet(ageModel,ageProto)
+    genderNet=cv2.dnn.readNet(genderModel,genderProto)
+    print('load gender & age successfully!!!')
+
+def highlightFace(net, frame, conf_threshold=0.7):
+    frameOpencvDnn=frame.copy()
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections=net.forward()
+    faceBoxes=[]
+    for i in range(detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence>conf_threshold:
+            x1=int(detections[0,0,i,3]*frameWidth)
+            y1=int(detections[0,0,i,4]*frameHeight)
+            x2=int(detections[0,0,i,5]*frameWidth)
+            y2=int(detections[0,0,i,6]*frameHeight)
+            faceBoxes.append([x1,y1,x2,y2])
+            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn,faceBoxes
+
+def gender_age(frame,faceNet,ageNet,genderNet):
+    global gender,age
+    padding = 20
+    resultImg, faceBoxes = highlightFace(faceNet, frame)
+    # if not faceBoxes:
+    #     print("No face detected")
+
+    for faceBox in faceBoxes:
+        face = frame[max(0, faceBox[1] - padding):
+                     min(faceBox[3] + padding, frame.shape[0] - 1), max(0, faceBox[0] - padding)
+                                                                    :min(faceBox[2] + padding, frame.shape[1] - 1)]
+
+        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        # gender_array.append(gender)
+
+        ageNet.setInput(blob)
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()]
+        # age_array.append(age)
+
+    return gender,age
+
 def main():
     # cap = cv2.VideoCapture("test_video.mp4")
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture('rtsp://admin:888888@192.168.7.50:10554/tcp/av0_0')
+    st = None
 
     while True:
         ret, frame = cap.read()
         if frame is None:
             continue
-        frame = cv2.resize(frame,(480,360))
-        (H, W) = frame.shape[:2]
+        if st == None:
+            st = time.time()
+        et = time.time()
+        if et - st > 0.2:
+            frame = cv2.resize(frame,(640,360))
+            (H, W) = frame.shape[:2]
 
-        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+            blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
 
-        detector.setInput(blob)
-        person_detections = detector.forward()
-        rects = []
-        for i in np.arange(0, person_detections.shape[2]):
-            # เป็น class อะไร
-            idx = int(person_detections[0, 0, i, 1])
-            if CLASSES[idx] != "person":
-                continue
-            # ความน่าจะเป็นของ class นั้นเท่าไหร่
-            confidence = person_detections[0, 0, i, 2]
-            if confidence > 0.6:
-                person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-                print(person_box)
-                rects.append(person_box)
-        # tracking config & non max suppression
-        print(rects)
-        boundingboxes = np.array(rects)
-        boundingboxes = boundingboxes.astype(int)
-        rects = non_max_suppression_fast(boundingboxes, 0.3)
-        print(rects)
-        objects = tracker.update(rects)
-        for (objectId, bbox) in objects.items():
-            x1, y1, x2, y2 = bbox
-            x1 = int(x1)
-            y1 = int(y1)
-            x2 = int(x2)
-            y2 = int(y2)
-            objectId = objectId+1
-            cv2.rectangle(frame, (x1-5, y1), (x2-5, y2), (0, 0, 255), 2)
-            text = "ID: {}".format(objectId)
-            cv2.putText(frame, text, (x1, y1-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+            detector.setInput(blob)
+            person_detections = detector.forward()
+            rects = []
+            for i in np.arange(0, person_detections.shape[2]):
+                # เป็น class อะไร
+                idx = int(person_detections[0, 0, i, 1])
+                if CLASSES[idx] != "person":
+                    continue
+                # ความน่าจะเป็นของ class นั้นเท่าไหร่
+                confidence = person_detections[0, 0, i, 2]
+                if confidence > 0.1:
+                    person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                    # print(person_box)
+                    rects.append(person_box)
+            # tracking config & non max suppression
+            # print(rects)
+            boundingboxes = np.array(rects)
+            boundingboxes = boundingboxes.astype(int)
+            rects = non_max_suppression_fast(boundingboxes, 0.3)
+            # print(rects)
+            objects = tracker.update(rects)
+            for (objectId, bbox) in objects.items():
+                x1, y1, x2, y2 = bbox
+                x1 = int(x1)
+                y1 = int(y1)
+                x2 = int(x2)
+                y2 = int(y2)
 
-        cv2.imshow("camera", frame)
-        k = cv2.waitKey(1)
-        if k == ord('q'):
-            break
+                frame_face = frame[y1:y2, x1:x2]
+                gender,age = gender_age(frame_face, faceNet, ageNet, genderNet)
+
+                objectId = objectId+1
+                cv2.rectangle(frame, (x1-5, y1), (x2-5, y2), (0, 0, 255), 2)
+                text = "ID: {} {} {}".format(objectId,gender,age)
+                cv2.putText(frame, text, (x1, y1-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+            st = time.time()
+            cv2.imshow("camera", frame)
+
+            k = cv2.waitKey(1)
+            if k == ord('q'):
+                break
     cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
+    load_all_model()
     # load model
     protopath = "camcount/MobileNetSSD_deploy.prototxt"
     modelpath = "camcount/MobileNetSSD_deploy.caffemodel"

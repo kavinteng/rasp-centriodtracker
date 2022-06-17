180140db80ca0288b66528dba2cf350fa29d4102
kavinteng <76694895+kavinteng@users.noreply.github.com>
2022-06-17 17:18:05 +0700

    Add files via upload

--- person_tracking.py
+++ person_tracking.py
@@ -95,8 +95,7 @@ def gender_age(frame,faceNet,ageNet,genderNet):
             face = frame[max(0, faceBox[1] - padding):
                          min(faceBox[3] + padding, frame.shape[0] - 1), max(0, faceBox[0] - padding)
                                                                         :min(faceBox[2] + padding, frame.shape[1] - 1)]
-            if face is not None:
-                print(face)
+            if len(face) != 0:
                 blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
                 genderNet.setInput(blob)
                 genderPreds = genderNet.forward()
